ba61e27cbcf00f240635b57319b1c4976b698050
safarilewis <lleewwii1@gmail.com>
2023-01-23 21:20:50 +0300

    update

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,24 +1,29 @@
-#!/usr/bin/pythons
-from flask import Flask, jsonify
+#!/usr/bin/python3
+"""
+This module runs the flask api on
+"""
+from flask import Flask, Blueprint, jsonify
+from flask_cors import CORS
 from models import storage
 from api.v1.views import app_views
 from os import getenv
-from flask_cors import CORS
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
-CORS(app, resources={"/*":{"origins" "0:0:0:0"}})
+CORS(app, resources={"/*": {"origins": "0.0.0.0"}})
 
 
 @app.teardown_appcontext
 def shut(exec):
-    """Removes storage session"""
+    """ remove storage session after request """
     storage.close()
+
 
 @app.errorhandler(404)
 def page_not_found(e):
-    """handles page not found"""
+    """ handle page not found error """
     return jsonify({"error": "Not found"}), 404
+
 
 if __name__ == "__main__":
     app.run(getenv('HBNB_API_HOST'), getenv('HBNB_API_PORT'), threaded=True)
